9871182e492743457b02dc07641c54e13bf52ae2
bakdoolot-kg <altybaevbaku15@gmail.com>
2024-02-14 18:31:25 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function getObj(objID)
+{
+    if (document.getElementById) {return document.getElementById(objID);}
+    else if (document.all) {return document.all[objID];}
+    else if (document.layers) {return document.layers[objID];}
+}
+var ie4=document.all;
+var ns6=document.getElementById&&!document.all;
+cobj=getObj("button1");
+
+function moveIt(){
+y=Math.floor(Math.random()*301);
+x=Math.floor(Math.random()*401);
+if (ie4){
+	cobj.style.top  = y;
+	cobj.style.left = x;
+	}
+else if (ns6){
+	cobj.style.top  = y+"px";
+	cobj.style.left = x+"px";
+	}
+}
